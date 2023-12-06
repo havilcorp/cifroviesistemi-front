9b8f76a0b5c7163a25c4f66a9ad3d98d305d58ba
havilcorp <havilcorp@gmail.com>
2023-12-06 18:31:40 +0300

    asd

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
 	transpileDependencies: true,
-	host: '0.0.0.0',
+	devServer: {
+		open: process.platform === 'darwin',
+		host: '0.0.0.0',
+		port: 8080,
+		https: false,
+		hotOnly: false,
+	},
 })
